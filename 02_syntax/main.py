18c8b15fe768d03e603300276ac47b8cb5187aca
bikasakumar <bikasakumarnayak@gmail.com>
2026-09-09 15:04:44 +0530

    initial changes

---
+++ 02_syntax/main.py
@@ -0,0 +1,9 @@
+print("Hello Python 2026")
+print("I am learning Python programming language")
+print("Python is a great language for beginners")
+print("I am enjoying learning Python")
+
+print("I will continue to practice and improve my skills in Python")
+
+
+print(3+5)
